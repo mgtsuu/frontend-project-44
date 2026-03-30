61896ae9370218370e2955dbcd064d1075459c52
mgtsuu <marrudiel@gmail.com>
2026-03-30 19:45:37 +0300

    fix

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -3,23 +3,15 @@
 import runGame from '../src/index.js'
 
 const isPrime = (num) => {
-  if (num < 2) {
-    return false
-  }
+  if (num < 2) return false
 
-  if (num === 2) {
-    return true
-  }
+  if (num === 2) return true
 
-  if (num % 2 === 0) {
-    return false
-  }
+  if (num % 2 === 0) return false
 
   const limit = Math.sqrt(num)
-  for (let i = 0; i <= limit; i += 2) {
-    if (num % i === 0) {
-      return false
-    }
+  for (let i = 3; i <= limit; i += 2) {
+    if (num % i === 0) return false
   }
   return true
 }
