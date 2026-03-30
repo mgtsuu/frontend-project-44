efbaa17f2d7593d370aacc1e42a6a9632c410142
mgtsuu <marrudiel@gmail.com>
2026-03-30 19:18:18 +0300

    step8

---
+++ bin/brain-progression.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+import runGame from '../src/index.js'
+
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
+
+const startRound = () => {
+  const length = getRandomNumber(5, 10)
+  const start = getRandomNumber(1, 20)
+  const step = getRandomNumber(1, 10)
+  const hiddenElement = getRandomNumber(0, length - 1)
+
+  const progression = []
+  for (let i = 0; i < length; i++) {
+    progression.push(start + i * step)
+  }
+
+  const correctAnswer = progression[hiddenElement]
+  progression[hiddenElement] = '..'
+
+  const question = progression.join(' ')
+  return { question, correctAnswer }
+}
+
+runGame({
+  description: 'What number is missing in the progression?',
+  startRound,
+})
